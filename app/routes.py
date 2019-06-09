88622d723a4e26ce5fa1f23eee94229e7d110f88
cwitt2013 <git.cw@posteo.de>
2019-06-09 12:52:02 +0200

    Jinja2 Test

--- app/routes.py
+++ app/routes.py
@@ -5,9 +5,20 @@ from flask.templating import render_template
 @app.route("/index")
 def index():
     user = {'username': 'Lena'}
+    posts = [
+        {
+            "author": {"username": "Christian"},
+            "body": "I tested this cool blog!",
+        },
+        {
+            "author": {"username": "Kira"},
+            "body": "This blog is lame!",
+        }
+    ]
     return render_template("index.html", 
                            title="Home",
-                           user=user)
+                           user=user,
+                           posts=posts)
 
 @app.route("/testing")
 def testing():
