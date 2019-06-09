7ef8499a45a62ac9531fc309230e6860b9d08809
cwitt2013 <git.cw@posteo.de>
2019-06-09 12:26:53 +0200

    Initial Commit microblog

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='microblog',
+      version='0.1',
+      description='microblog tutorial',
+      author='CW',
+      packages=['flask'],
+     )
